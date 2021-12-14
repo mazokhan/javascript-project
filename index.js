e334cd4f36c93765bda925eac297faf599d5fd93
Mazen <92783879+mazokhan@users.noreply.github.com>
2021-12-14 17:57:23 +1100

    Update index.js

--- index.js
+++ index.js
@@ -9,14 +9,12 @@ async function fetchAllCoins() {
     }
 }
 
-
-
 // Fetch and Populate API Data in HTML
 async function showAllCoins() {
     let coins = await fetchAllCoins();
     let html = '';
     coins.forEach(coin => {
-        let htmlSegment = `<div class="card-desc">
+        let htmlSegment = `<div class="card-desc" onclick="handleClick('${coin.id}')">
                                 <img src="${coin.image}" width="100">
                                 <div class="desc-border">
                                     <h3>${coin.id}, ${coin.symbol}</h3>
@@ -31,11 +29,24 @@ async function showAllCoins() {
 }
 showAllCoins();
 
+//
+async function handleClick(coinID) {
+    let coins = await fetchAllCoins();
+    let coinData = coins.filter(data => {
+        return data.id === coinID
+    })
+    console.log("filteredData", coinData)
+    var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < coinData.length; i++) {
+        let list = document.createElement('tr');
+        list.innerHTML = "<td>" + `<img src=${coinData[i].image} width="100">` + "</td>" + "<td>" + `<input type="text" value="${coinData[i].id}" required>` + "</td>" + "<td>" + `<input type="text" value="${coinData[i].symbol}" required>` + "</td>" + "<td>" + `<input type="text" value="${coinData[i].name}" required>` + "</td>" + "<td>" + `<input type="number" value="${coinData[i].current_price}" step="any" required>` + "</td>" + "<td>" + `<button onclick="updateCoin(${coinData[i].id});" class="btn btn-success">Update</button>` + "</td>" + "<td>" + `<button onclick="deleteCoin('${i}','${coinData[i].id}');" class="btn btn-danger">Delete</button>` + "</td>";
+        mainContainer.appendChild(list);
+    }
+}
 
-
-// Add Data from HTML to JSON
+//Add Data from HTML to JSON
 let form = document.getElementById("form");
-form.addEventListener("submit", function(e) {
+form.addEventListener("submit", function (e) {
     e.preventDefault()
     let coinImage = document.getElementById("imageurl").value;
     let coinId = document.getElementById("id").value;
@@ -58,64 +69,13 @@ form.addEventListener("submit", function(e) {
             cp: coinPrice
         })
     })
-    .then(function(response) {
-        return response.json()
-    })
-    .then(function(data) {
-        console.log(data)
-    })
-})
-
-
-
-// Fetch and Populate JSON Data in HTML
-fetch('http://localhost:3000/posts').then(function(response) {
-    return response.json();
-})
-.then(function(data) {
-    showMyCoins(data);
-})
-.catch(function(error) {
-    console.log('error: ' + error);
-});
-function showMyCoins(data) {
-    var mainContainer = document.getElementById("myData");
-    for (var i = 0; i < data.length; i++) {
-        let list = document.createElement('tr');
-        list.innerHTML = "<td>"+`<img src=${data[i].cimg} width="100">`+"</td>" + "<td>" + `<input type="text" value="${data[i].cid}" required>` + "</td>" + "<td>" + `<input type="text" value="${data[i].cs}" required>` + "</td>" + "<td>" + `<input type="text" value="${data[i].cn}" required>` + "</td>" + "<td>" + `<input type="number" value="${data[i].cp}" step="any" required>` + "</td>" + "<td>" + `<button onclick="updateCoin(${data[i].id});" class="btn btn-success">Update</button>` + "</td>" + "<td>" + `<button onclick="deleteCoin(${data[i].id});" class="btn btn-danger">Delete</button>` + "</td>";
-        mainContainer.appendChild(list);
-    }
-}
-
-
-
-// Update the Populated Row HTML
-function updateCoin(id) {
-    let coinId = document.getElementById("id").value;
-    let coinSymbol = document.getElementById("symbol").value;
-    let coinName = document.getElementById("name").value;
-    let coinPrice = document.getElementById("price").value;
-    return fetch(`http://localhost:3000/posts/${id}`, {
-        method: "PUT",
-        headers: {
-            "Content-Type": "application/json; charset=UTF-8"
-        },
-        body: JSON.stringify({
-            id: id,
-            cimg: coinImage,
-            cid: coinId,
-            cs: coinSymbol,
-            cn: coinName,
-            cp: coinPrice
+        .then(function (response) {
+            if (response.status == 201) {
+                form.reset();
+            }
+            return response.json()
         })
-    })
-}
-
-
-
-// Delete the Populated Data Row HTML
-function deleteCoin(id) {
-    return fetch(`http://localhost:3000/posts/${id}`, {
-        method: "DELETE"
-    })
-}
+        .then(function (data) {
+            console.log(data)
+        })
+});
